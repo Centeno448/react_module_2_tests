87fc4db1c684fe878333d87aaca465b22e8e3e78
Centeno448 <diego.centeno@udla.edu.ec>
2021-05-28 20:51:34 -0500

    fixed formatting

--- src/TareasLista.test.js
+++ src/TareasLista.test.js
@@ -1,8 +1,7 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow } from 'enzyme';
-import TareasLista from "./TareasLista"
-
+import TareasLista from './TareasLista';
 
 beforeAll(() => {
   configure({ adapter: new Adapter() });
@@ -10,13 +9,13 @@ beforeAll(() => {
 
 test('TareasLista tiene 1 <div> | Asegúrate de que tengas 1 <div> dentro de TareasListas.js', () => {
   const wrapper = shallow(<TareasLista />);
-  const div = wrapper.find("div");
+  const div = wrapper.find('div');
   expect(div).toBeDefined();
   expect(div.length).toBe(1);
 });
 
 test('<div> de TareasLista contiene el texto "Hello World" | Asegúrate de que dentro del <div> de TareasLista escribas "Hello World"', () => {
   const wrapper = shallow(<TareasLista />);
-  const div = wrapper.find("div").first();
-  expect(div.html()).toContain("Hello World");
+  const div = wrapper.find('div').first();
+  expect(div.html()).toContain('Hello World');
 });
